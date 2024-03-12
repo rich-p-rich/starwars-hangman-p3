af4edd23eec1be7356dc74c3565639e63e30a7e9
Rich Ricot <richricot@gmail.com>
2024-03-12 14:18:17 +0000

    Update all instances of print with myprint to improve readability in the terminal

--- run.py
+++ run.py
@@ -47,13 +47,13 @@ def how_to_play():
     myprint('how many attempts you have left.\n')
     myprint('If you get stuck, you can ask for up to two clues. Each clue will\n') 
     myprint('use up one attempt.\n')
-    print('Clue one will tell you whether the name is a character, a ship, and so on.\n')
-    print('Clue two will tell you what set of films and / or TV series they are seen most in.\n')
-    print('_ _ E   _ E _ _ _   _ _ _ _\n')
-    print('Clue one: a place.\n')
-    print('_ H E   _ E _ _ H   _ _ _ R\n')
-    print('Clue two: the Original Trilogy.\n')
-    print('T H E   D E A T H   S T A R \n')
+    myprint('Clue one will tell you whether the name is a character, a ship, and so on.\n')
+    myprint('Clue two will tell you what set of films and / or TV series they are seen most in.\n')
+    myprint('_ _ E   _ E _ _ _   _ _ _ _\n')
+    myprint('Clue one: a place.\n')
+    myprint('_ H E   _ E _ _ H   _ _ _ R\n')
+    myprint('Clue two: the Original Trilogy.\n')
+    myprint('T H E   D E A T H   S T A R \n')
     input('Press enter to play the game:  \n')
     ("Starting your attack run ...\n")
     play_the_game()
@@ -81,34 +81,34 @@ def play_the_game():
     else:   
         attempts = 10
 
-    print("Your target:", ' '.join(hidden_word))
-    print('\n')
-    print("You have",attempts,"shots to save your planet.")
-    print('\n')
+    myprint("Your target:", ' '.join(hidden_word))
+    myprint('\n')
+    myprint("You have",attempts,"shots to save your planet.")
+    myprint('\n')
 
     while attempts > 0:
         guess = input("Take a shot! Guess a letter: ")
         if guess in word:
-            print("Great shot!")
+            myprint("Great shot!")
         else:
-            print("Just missed! Try again: you have", attempts, "shots left")
+            myprint("Just missed! Try again: you have", attempts, "shots left")
             attempts -= 1 
         for i, letter in enumerate(word):
             if letter == guess:
                 hidden_word[i] = guess
-        print("Your target:", ' '.join(hidden_word))
+        myprint("Your target:", ' '.join(hidden_word))
         if '_' not in hidden_word:
-            print("Great shot kid! That was one in a million:", word)
-            print("Don't recognise the answer? Look it up on Wookipedia: https://starwars.fandom.com/wiki/Main_Page')")
+            myprint("Great shot kid! That was one in a million:", word)
+            myprint("Don't recognise the answer? Look it up on Wookipedia: https://starwars.fandom.com/wiki/Main_Page')")
             input('Press 1 and enter to start a new game:  \n')
             ("Starting your attack run ...\n")
             play_the_game()
             break
 
     if attempts: 0
-    print("Oh no! The Death Star has won!")
-    print("The answer was:", word)
-    print("Don't recognise the answer? Look it up on Wookipedia: https://starwars.fandom.com/wiki/Main_Page')")
+    myprint("Oh no! The Death Star has won!")
+    myprint("The answer was:", word)
+    myprint("Don't recognise the answer? Look it up on Wookipedia: https://starwars.fandom.com/wiki/Main_Page')")
     input('Press 1 and enter to play the game:  \n')
     ("Starting your attack run ...\n")
     play_the_game()
@@ -119,23 +119,23 @@ Set up homescreen
 The user chooses to play the game or read the gameplay instructions 
 """
 def display_homescreen():
-    print('Welcome to Starwars Hangman!')
-    print('Save your homeworld by guessing the missing name before the Death Star is in range!\n')
-    print('Choose your option:\n')
+    myprint('Welcome to Starwars Hangman!')
+    myprint('Save your homeworld by guessing the missing name before the Death Star is in range!\n')
+    myprint('Choose your option:\n')
     option1 = 'Play the game: press "1" and enter.\n'
     option2 = 'How to play: press "2" and enter.\n'
-    print(option1)
-    print(option2)
+    myprint(option1)
+    myprint(option2)
     option = input("Enter your option:  ")
 
     if option == '1':
-        print("Starting your attack run ...\n")
+        myprint("Starting your attack run ...\n")
         play_the_game()
     elif option == '2':
-        print("Loading up the Death Star plans now ...\n")
+        myprint("Loading up the Death Star plans now ...\n")
         how_to_play() 
     else:
-        print("No target found: please enter 1 or 2.\n")
+        myprint("No target found: please enter 1 or 2.\n")
 
 display_homescreen()
 
